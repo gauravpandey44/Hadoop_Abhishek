8e56011d107af385b7f7af012fec8fa25c2a496f
Gaurav Pandey <gauravpandey44@gmail.com>
2015-11-27 17:38:27 +0530

    updated

---
+++ src/JavaPractice/Swap2Numbers.java
@@ -0,0 +1,20 @@
+package JavaPractice;
+
+public class Swap2Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number1=10;
+		int number2=20;
+		System.out.println("\nIntially the numbers are :\n number1=> "+number1+"\n number2=> "+number2);
+		int tmp=number1;
+		number1=number2;
+		number2=tmp;
+		System.out.println("\nNow SWAPPING NUMBERS\n");
+		System.out.println("\nAfter swappping numbers are :\n number1=> "+number1+"\n number2=> "+number2);
+		
+
+	}
+
+}
