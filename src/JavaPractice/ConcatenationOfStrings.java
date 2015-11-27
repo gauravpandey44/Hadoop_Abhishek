5c99b9b9a75c536bdb356e7d38ae2bebc26e379c
Gaurav Pandey <gauravpandey44@gmail.com>
2015-11-27 18:10:19 +0530

    updated

---
+++ src/JavaPractice/ConcatenationOfStrings.java
@@ -0,0 +1,21 @@
+package JavaPractice;
+
+public class ConcatenationOfStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String string1="I LOVE";
+		String string2="MY ";
+		String string3="INDIA";
+		String concatenatedString=string1+string2+string3;
+		System.out.println("String1: "+string1);
+		System.out.println("String2: "+string2);
+		System.out.println("String3: "+string3);
+		System.out.println("Concatenation of strings is: "+concatenatedString);
+		
+		
+		
+	}
+
+}
