ef09689c5c5affdb733d63d3567d978bcc42e202
Gaurav Pandey <gauravpandey44@gmail.com>
2015-11-27 18:34:16 +0530

    updated

---
+++ src/JavaPractice/CheckingDuplicateInArray.java
@@ -0,0 +1,38 @@
+package JavaPractice;
+
+public class CheckingDuplicateInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		int array[]={10,20,30,40,20,99,35,30,20};
+		boolean flag=false;
+		for(int i=0;i<array.length;i++) {
+			
+			flag=false;
+			
+			for(int j=0;j<array.length;j++) {
+				
+				if(i!=j) {
+				
+					if(array[i]==array[j]) {
+						
+						flag=true;
+						continue;
+					}
+					
+				}
+				
+			}
+			if(flag) {
+				
+				System.out.println("\n Duplicate element  "+array[i]+" found ");
+			}
+			
+		}
+		
+		
+		
+	}
+
+}
